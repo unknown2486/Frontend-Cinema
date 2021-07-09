140725fd036202e3352679dee73acedc93f765a0
ILIAS ASSAD <ASSAD_ILIAS@emsi-edu.ma>
2021-07-10 00:33:57 +0200

    Second commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Cinema-frontend'`, () => {
+  it(`should have as title 'Cinema-Front-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Cinema-frontend');
+    expect(app.title).toEqual('Cinema-Front-web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Cinema-frontend app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Cinema-Front-web app is running!');
   });
 });
